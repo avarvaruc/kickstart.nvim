304f4a9bcbe99b83b4ab3b34875ec1b14b2a7a19
Andrei Varvaruc <aavarvaruc@wiley.com>
2025-03-23 16:36:20 +0200

    Add auto-session

--- init.lua
+++ init.lua
@@ -646,6 +646,18 @@ require('lazy').setup({
     },
   },
   {
+    'rmagatti/auto-session',
+    lazy = false,
+
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+      suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+      -- log_level = 'debug',
+    }
+  },
+  {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
     dependencies = {
